f038d2ba0e3a93c504d29f3f94a001491b3c474f
Cecelia <CeceliaPendell@gmail.com>
2024-10-19 15:48:20 -0600

    started

--- RecipeParty/Program.cs
+++ RecipeParty/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+        List<string> mealOptions = new List<string>();
+        mealOptions.Add("Breakfast");
+        mealOptions.Add("Dinner");
+        mealOptions.Add("Snack");
+        mealOptions.Add("Dessert");
+
+
+    }
+}
+
+class Recipe
+{
+    string name = "";
+    bool hasMade = false;
+    Dictionary<string, string> ingredients = new Dictionary<string, string>();
+    string mealType = "";
+    List<string> directions = new List<string>();
+
+    static void getName()
+    {
+        Console.WriteLine("Enter Recipe Name: ");
+        //self.name = Console.ReadLine();
+    }
+    
+}
+
